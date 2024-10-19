9db3251d6f30d53afbc9525ab2d72d84ace2d8a4
geeuleeo <giulio.vacca@edu.itspiemonte.it>
2024-10-19 21:19:55 +0200

    Gestione messaggi di errori pagina di registrazione

--- BitWizard/src/main/java/com/wizard/templateController/GlobalExceptionHandler.java
+++ BitWizard/src/main/java/com/wizard/templateController/GlobalExceptionHandler.java
@@ -1,12 +1,17 @@
 package com.wizard.templateController;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.wizard.exceptions.EmailAlreadyExistsException;
 import com.wizard.exceptions.RuoloNotFoundException;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -28,5 +33,20 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public String handleGenericException(Exception ex) {
         return "Errore generico: " + ex.getMessage();
+    }
+
+
+    // Gestione degli errori di inserimento dati
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<String>> handleValidationExceptions(MethodArgumentNotValidException errore) {
+        // Estrai i messaggi di errore predefiniti
+        List<String> errorMessages = errore.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getDefaultMessage()) // Estrai solo il messaggio predefinito
+                .collect(Collectors.toList());
+
+        // Restituisci i messaggi di errore con lo stato HTTP 400 Bad Request
+        return new ResponseEntity<>(errorMessages, HttpStatus.BAD_REQUEST);
     }
 }
